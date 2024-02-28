4d5a2f3c6abe19cd2204ac7b9c55f5c97e4a35ca
Afonso Gouveia <gouvei44@gmail.com>
2024-02-27 22:49:15 -0300

    feat: interface user

---
+++ src/types/user.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+    id?: string;
+    name?: string;
+    email?: string;
+    active?: boolean;
+    phone?: string;
+    password?: string;
+  }
+  
+  export interface IUserType {
+    id: string;
+    name: string;
+  }
